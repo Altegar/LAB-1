25ce825eec1efa820d2b2ee58b462c3a59e66df8
Altegar <91095463+Altegar@users.noreply.github.com>
2023-03-31 12:03:00 +0300

    Some changes

--- main.py
+++ main.py
@@ -6,11 +6,21 @@
 from math import sin, cos, log10
 
 
-def calculate_expression(x):
-    result = 2 * (x ** 2 - 3) - (sin(x ** 2) / 1 + cos(x)) + (cos(abs(x)) ** 2) - log10(x + 2) - 1 + sin(x) ** 2
-    return result
+def math_expression(x):
+    return 2 * (x ** 2 - 3) - (sin(x ** 2) / 1 + cos(x)) + (cos(abs(x)) ** 2) - log10(x + 2) - 1 + sin(x) ** 2
 
 
 y = float(input("Введіть, будь ласка, значення для x = "))
 
-print(f"y = {calculate_expression(y)}")
+print(f"1) y = {math_expression(y)}")
+
+a = int(input("Введіть початок проміжку: a = "))
+b = int(input("Введіть кінець проміжку: b = "))
+h = int(input("Введіть крок проміжку: h = "))
+
+lst = []
+for x in range(a, b + 1, h):
+    y = 2 * (x ** 2 - 3) - (sin(x ** 2) / 1 + cos(x)) + (cos(abs(x)) ** 2) - log10(abs(x) + 2) - 1 + sin(x) ** 2
+    lst.append(round(y, 3))
+
+print(f"2) y = {lst}")
